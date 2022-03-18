75a864eee8aeb838ccd977efad981780d2046ed6
Patrick Cerny <patrick.cerny04@gmail.com>
2022-03-18 10:46:23 +0100

    hopefully this works

--- src/utils/stores/windowStore.ts
+++ src/utils/stores/windowStore.ts
@@ -1,7 +1,6 @@
 import { makeAutoObservable } from 'mobx';
 import React from 'react';
 import ProgramsEnum from '../enums/programsEnum';
-import WindowStoreType from '../types/WindowStoreType';
 
 class WindowStore {
   storeKey: string = 'windowStore' as const;
